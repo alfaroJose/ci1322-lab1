489b8dbad32b08988e8b08af061e500dad8f141a
alfaroJose <josealfarow@gmail.com>
2019-04-26 04:08:25 -0600

    Speadsheet and classes

--- compiladores-laboratorio1/src/main/java/ucr/ac/ecci/ci1322/laboratorio1/model/Book.java
+++ compiladores-laboratorio1/src/main/java/ucr/ac/ecci/ci1322/laboratorio1/model/Book.java
@@ -1,4 +1,55 @@
 package ucr.ac.ecci.ci1322.laboratorio1.model;
 
 public class Book {
+
+    private int code;
+    private String author;
+    private String title;
+    private String genre;
+
+    public Book(int code, String author, String title, String genre) {
+        this.code = code;
+        this.author = author;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Book(){
+        this.code = 0;
+        this.author = "";
+        this.title = "";
+        this.genre = "";
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
--- compiladores-laboratorio1/src/main/java/ucr/ac/ecci/ci1322/laboratorio1/model/Student.java
+++ compiladores-laboratorio1/src/main/java/ucr/ac/ecci/ci1322/laboratorio1/model/Student.java
@@ -1,4 +1,66 @@
 package ucr.ac.ecci.ci1322.laboratorio1.model;
 
 public class Student {
+
+    private String studenId;
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String email;
+
+    public Student(String studenId, int id, String lastName, String firstName, String email) {
+        this.studenId = studenId;
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public Student() {
+        this.studenId = "";
+        this.id = 0;
+        this.lastName = "";
+        this.firstName = "";
+        this.email = "";
+    }
+
+    public String getStudenId() {
+        return studenId;
+    }
+
+    public void setStudenId(String studenId) {
+        this.studenId = studenId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
